577f2904e681c0352739a4506ece8312fadf2813
Lidor Iluz <lidoryoni@gmail.com>
2021-03-24 17:44:32 +0200

    first

---
+++ text.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+File=open("/Users/lidoriluz/Desktop/matalot/matala2/text.txt")
+
+
+def revword(word:str):
+    word=word[::-1]
+    word=word.lower()
+    return word
+        
+ 
+def countword(File):
+    for line in File:
+        line = line.strip("\n")
+        first=line
+        break
+
+    
+    count=1
+    for line in File:
+        line = line.strip("\n")
+        words = line.split()
+        for z in words:
+            revword(z)
+            if first==revword(z):
+                count+=1
+    print("Count of word=",count)
+    
+    
+countword(File) 
+
+
+
+    
+
